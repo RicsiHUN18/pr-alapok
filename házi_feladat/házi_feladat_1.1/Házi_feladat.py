8401b1ec88748a3639a3479bb84cc317f34547ad
BarnaR <113334754+RicsiHUN18@users.noreply.github.com>
2023-09-25 12:53:22 +0200

    Update Házi_feladat.py

--- házi_feladat/házi_feladat_1.1/Házi_feladat.py
+++ házi_feladat/házi_feladat_1.1/Házi_feladat.py
@@ -1,9 +1,8 @@
-# házi feladat 
+# házi feladat 1.1
 
 válasz = input("Jó napja van? ")
 if válasz == "igen":
     print("Örülök, hogy jó napja van, remélem több ilyen lesz!")
 elif válasz == "nem":
     print("Sajnálom, remélem holnap jobb napja lesz!")
-else:
-    print("Sajnos nem értem a válaszodat.")
+
