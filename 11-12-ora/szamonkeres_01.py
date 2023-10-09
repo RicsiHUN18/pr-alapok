97de3fdc09dae3ca7edf9918f8cd266b4d376773
BarnaR <113334754+RicsiHUN18@users.noreply.github.com>
2023-10-09 13:07:38 +0200

    Add files via upload

---
+++ 11-12-ora/szamonkeres_01.py
@@ -0,0 +1,6 @@
+print("Barna Richárd István")
+jegy = int(input("Kérek egy jegyet 1-5:"))
+if jegy == 5: 
+    print(f'A jegyed:', jegy)
+elif jegy < 5:
+    print(f"A jegyed:, {jegy} Ami nem megfelelő!")
