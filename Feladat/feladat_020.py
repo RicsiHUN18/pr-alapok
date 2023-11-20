fc9f78e426aa77e3ad29edfc1e702c623ef82c3b
BarnaR <113334754+RicsiHUN18@users.noreply.github.com>
2023-11-20 12:55:49 +0100

    Add files via upload

---
+++ Feladat/feladat_020.py
@@ -0,0 +1,11 @@
+# a kiírást követően a kurzor egy tabulátornyit ugrik
+print('Teszt', end='\t')
+print('Teszt', end='\t')
+print('Teszt', end='\t')
+
+print()
+
+# a kiíratás követően a kurzor a kiírás vlgln marad 
+print('Teszt', end='')
+print('Teszt', end='')
+print('Teszt', end='')
