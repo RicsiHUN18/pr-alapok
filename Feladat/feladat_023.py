af65a55a42a56f5746229a81dc25691d0af8c435
BarnaR <113334754+RicsiHUN18@users.noreply.github.com>
2023-11-20 14:20:49 +0100

    Add files via upload

---
+++ Feladat/feladat_023.py
@@ -0,0 +1,12 @@
+# feladat_023
+
+honapok = ['január', 'február', 'március','április', 'május']
+"""
+lista = [1, 2, 3, 4]
+"""
+print(honapok)
+# print((' ').join(honapok))
+
+print(len(honapok))  #Len = hossz
+#print(honapok[5]
+print(f'A lista utolsó legnagyobb indexe: {len(honapok) - 1}')
